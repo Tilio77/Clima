73e2e7a43414d8d975efa7fca3789eccadac67ad
Tilio77 <tiliobolso1@gmail.com>
2022-09-22 00:12:30 -0300

    Buscador de clima

---
+++ src/components/weatherMainInfo.js
@@ -0,0 +1,32 @@
+import styles from './weatherMainInfo.module.css';
+
+export default function WeatherMainInfo({weather}) {
+	return (
+		<div className={styles.mainInfo}>
+			<div className={styles.city}>{weather?.location.name}</div>
+			<div className={styles.country}>{weather?.location.country}</div>
+			<div className={styles.row}>
+				<div>
+					<img 
+						src={`http:${weather?.current.condition.icon}`} 
+						width="128" 
+						alt={weather?.current.condition.text} 
+					/>
+				</div>
+				<div className={styles.weatherConditions}>
+					<div className={styles.condition}>{weather?.current.condition.text}</div>
+					<div className={styles.current}>{weather?.current.temp_c}°</div>
+				</div>	
+			</div>
+			<iframe 
+				title="mapa"
+				src={`https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d318687.3901668895!2d${weather?.location.lon}657!3d${weather?.location.lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2suy!4v1663777630056!5m2!1ses-419!2suy`}
+				width="100%" 
+				height="450" 
+				style={{ border:0 }} 
+				loading="lazy" 
+				referrerpolicy="no-referrer-when-downgrade">
+			</iframe>
+		</div>
+		);
+	}
